073f3968fa1cfa0e1baa0a38cc5e96a575ba3761
Alban316 <jason.brugger@gmail.com>
2016-02-23 14:03:59 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq2
+{
+    class Program
+    {
+        static void GetPeople(string match)
+        {
+            AwDbDataContext db = new AwDbDataContext();
+
+            var query =
+                from rowset in db.Persons
+                where rowset.LastName.StartsWith(match)
+                select rowset;
+
+            foreach (Person person in query)
+            {
+                Console.WriteLine("{0}, {1} {2}", person.LastName, person.FirstName, person.MiddleName);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the search string:");
+            string search = Console.ReadLine();
+            GetPeople(search);
+        }
+    }
+}
